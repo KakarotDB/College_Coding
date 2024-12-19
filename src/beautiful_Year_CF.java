656c3826e049cc173e2888ec4f5aa496bb2ea84a
Mrigaj <mrigajpersonal@gmail.com>
2024-12-19 13:36:14 +0530

    Added interesting_robbery_cf from IIEST GROUP contest Also edited palindrome_Check.java file using two pointer method as it is more efficient

---
+++ src/beautiful_Year_CF.java
@@ -0,0 +1,14 @@
+import java.util.OptionalInt;
+import java.util.Scanner;
+
+public class beautiful_Year_CF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (Integer.toString(++t).chars().distinct().count()<4)
+        {}
+        System.out.println(t);
+        
+
+    }
+}
