10d678c5c64aee83884d20a463d87d49f46f48bb
KakarotDB <mrigajpersonal@gmail.com>
2025-07-01 19:55:10 +0530

    More practice lol

--- Codeforces - Codeforces Round 742 Div 2/B. MEXor Mixup/Main.java
+++ Codeforces - Codeforces Round 742 Div 2/B. MEXor Mixup/Main.java
@@ -15,7 +15,21 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
+            int xor = 0;
+            if ((a - 1) % 4 == 0)
+                xor = a - 1;
+            else if ((a - 1) % 4 == 1)
+                xor = 1;
+            else if ((a - 1) % 4 == 2)
+                xor = a;
+            else if ((a - 1) % 4 == 3)
+                xor = 0;
+            if (xor == b)
+                pw.println(a);
+            else if ((xor ^ b) != a)
+                pw.println(a + 1);
+            else
+                pw.println(a + 2);
         }
         pw.flush();
         pw.close();
