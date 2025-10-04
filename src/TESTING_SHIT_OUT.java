e6641cc6013153b2c8d6f74898dc0a5b43143f8d
KakarotDB <mrigajpersonal@gmail.com>
2025-10-04 17:14:44 +0530

    More practice lol

--- src/TESTING_SHIT_OUT.java
+++ src/TESTING_SHIT_OUT.java
@@ -1,25 +1,110 @@
-import java.util.Arrays;
+//  E. Mexification
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+import static java.lang.Math.*;
 
 public class TESTING_SHIT_OUT {
-    public static void main(String[] args) {
-        int n = 5;
-        long[] memo = new long[n + 1];
-        System.out.println(fibonacci_With_Memoization(n, memo));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            long k = Long.parseLong(st.nextToken());
+            Map<Integer, Integer> fmap = new HashMap<>();
+            Set<Integer> set = new HashSet<>();
+            st = new StringTokenizer(br.readLine());
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(st.nextToken());
+                set.add(a[i]);
+            }
+            int mex = 0;
+            int sum = 0;
+            for (int i = 0; i <= n; i++) {
+                if (!set.contains(i)){
+                    mex = i;
+                    break;
+                }
+            }
+            Arrays.sort(a);
+            if (mex < a[n - 1]) {
+                for (int i = 0; i < n; i++) {
+                    if (a[i] > mex)
+                        a[i] = mex;
+                    fmap.put(a[i], fmap.getOrDefault(a[i], 0) + 1);
+                }
+                if (k % 2 == 1) {
+                    for (int i = 0; i < n; i++) {
+                        sum += a[i];
+                    }
+                }
+                else {
+                    for (int i = 0; i < n; i++) {
+                        if (a[i] == mex && fmap.get(a[i]) != 1) a[i] ++;
+                        sum += a[i];
+                    }
+                }
 
-    }
-    public static boolean check_power_of_2(int n){
-        int check = n & (n - 1);
-        return check == 0;
-    }
-    public static long fibonacci_With_Memoization(int n, long[] memo) {
-        long result = 0;
-        if (memo[n] != 0)
-            return memo[n];
-        if (n == 1 || n == 2)
-            result = 1;
-        else
-            result = fibonacci_With_Memoization(n - 1, memo) + fibonacci_With_Memoization(n - 2, memo);
-        memo[n] = result;
-        return result;
+            }
+            else {
+                boolean flag = true;
+                for (int i = 0; i < n; i++) {
+                    fmap.put(a[i], fmap.getOrDefault(a[i], 0) + 1);
+                }
+                for (int i = 0; i < n; i++) {
+                    if (fmap.get(a[i]) > 1) flag = false;
+                }
+                if (flag) {
+                    for (int i = 0; i < n; i++) {
+                        sum += a[i];
+                    }
+                }
+                else {
+                    for (int i = 0; i < n; i++) {
+                        if (fmap.get(a[i]) > 1) a[i] = mex;
+                    }
+                    if (k == 1) {
+                        for (int i = 0; i < n; i++) {
+                            sum += a[i];
+                        }
+                    }
+                    else {
+                        for (int i = 0; i < n; i++) {
+                            if (a[i] != i) {
+                                mex = i;
+                                break;
+                            }
+                        }
+                        for (int i = 0; i < n; i++) {
+                            if (a[i] > mex)
+                                a[i] = mex;
+                        }
+                        if (k % 2 == 0) {
+                            for (int i = 0; i < n; i++) {
+                                sum += a[i];
+                            }
+                        }
+                        else {
+                            for (int i = 0; i < n; i++) {
+                                if (a[i] == mex) a[i]++;
+                                sum += a[i];
+                            }
+                        }
+                    }
+                }
+            }
+
+            pw.println(sum);
+        }
+        pw.flush();
+        pw.close();
+        br.close();
+
     }
 }
